64e4fd80ed8357675320279e013c880fd2bc21e7
Agnel Selvan <agnelselvan007@gmail.com>
2026-04-17 06:08:03 +0530

    Add files via upload

---
+++ _next/static/gOLNNricAO7Nthnmh2LAI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/personal_website//_next/:path+",
+        "destination": "/personal_website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
